53401e9f07ec13cde399258bd9f7191666d31332
DenisZhmakin <den.zhmakin.00@gmail.com>
2024-11-25 08:51:09 +0300

    feat: Рефакторинг

--- src/main/kotlin/ru/xdragon/carsgrpcsevicebackend/CarsGrpcSeviceBackendApplication.kt
+++ src/main/kotlin/ru/xdragon/carsgrpcsevicebackend/CarsGrpcSeviceBackendApplication.kt
@@ -1,11 +1,44 @@
 package ru.xdragon.carsgrpcsevicebackend
 
+import io.grpc.Server
+import io.grpc.ServerBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+
+import ru.xdragon.carsgrpcsevicebackend.service.CarTypeService
 
 @SpringBootApplication
 class CarsGrpcSeviceBackendApplication
 
+class GrpcServer(private val port: Int) {
+	private val server: Server = ServerBuilder.forPort(port)
+		.addService(CarTypeService())
+		.build()
+
+	fun start() {
+		server.start()
+		println("Server started, listening on $port")
+		Runtime.getRuntime().addShutdownHook(Thread {
+			println("Received Shutdown Request")
+			this.stop()
+			println("Successfully stopped the server")
+		})
+	}
+
+	private fun stop() {
+		server.shutdown()
+	}
+
+	fun blockUntilShutdown() {
+		server.awaitTermination()
+	}
+}
+
+
 fun main(args: Array<String>) {
 	runApplication<CarsGrpcSeviceBackendApplication>(*args)
+
+	val server = GrpcServer(9090)
+	server.start()
+	server.blockUntilShutdown()
 }
--- src/main/kotlin/ru/xdragon/carsgrpcsevicebackend/stub/CarTypeService.kt
+++
@@ -1,4 +0,0 @@
-package ru.xdragon.carsgrpcsevicebackend.stub
-
-class CarTypeService {
-}
